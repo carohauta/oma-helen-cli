38dde3a841584c126d79b6f6647a3d23f6f3d0dc
Caro Hautamäki <caro.hautamaki@bradleysdigital.fi>
2023-01-19 13:25:12 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 requirements = [
     'beautifulsoup4==4.11.1',
     'python-dateutil==2.8.2',
-    'requests==2.28.1',
+    'requests>=2.28.1',
     'soupsieve==2.3.2.post1',
     'urllib3==1.26.12',
     'cachetools==5.2.0'
@@ -22,14 +22,16 @@ setup(
     author_email='carosoft.dev@gmail.com',
     python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="Oma Helen API library and CLI",
     entry_points={
