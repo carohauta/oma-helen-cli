2ddaab70dcee6db5356b996e0cf21312b2a55138
Caro Hautamäki <caro.hautamaki@bradleysdigital.fi>
2023-09-07 10:36:07 +0300

    fix: accept unknown keywords in api responses

--- helenservice/api_response.py
+++ helenservice/api_response.py
@@ -2,13 +2,13 @@ from typing import List
 
 
 class Measurement(object):
-    def __init__(self, value: float, status: str):
+    def __init__(self, value: float, status: str, **_):
         self.value = value
         self.status = status
 
 
 class MeasurementData(object):
-    def __init__(self, start: str, stop: str, resolution_s: str, resolution: str, unit: str, measurements: List):
+    def __init__(self, start: str, stop: str, resolution_s: str, resolution: str, unit: str, measurements: List, **_):
         self.start = start
         self.stop = stop
         self.resolution_s = resolution_s
@@ -18,7 +18,7 @@ class MeasurementData(object):
 
 
 class MeasurementIntervals(object):
-    def __init__(self, electricity: List = None, electricity_transfer: List = None):
+    def __init__(self, electricity: List = None, electricity_transfer: List = None, **_):
         if electricity is not None:
             self.electricity = list(map(lambda e: MeasurementData(**e), electricity))
         elif electricity_transfer is not None:
@@ -26,10 +26,10 @@ class MeasurementIntervals(object):
 
 
 class MeasurementResponse(object):
-    def __init__(self, intervals: dict):
+    def __init__(self, intervals: dict, **_):
         self.intervals = MeasurementIntervals(**intervals) if intervals is not None else None
 
 
 class SpotPricesResponse(object):
-    def __init__(self, interval: dict):
+    def __init__(self, interval: dict, **_):
         self.interval = MeasurementData(**interval) if interval is not None else None
