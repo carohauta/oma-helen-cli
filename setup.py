53c2082296f4cc398b710c9499448304ab7b3897
Caro Hautamäki <caro.hautamaki@bradleysdigital.fi>
2025-03-24 09:49:14 +0200

    chore: bump up to version 1.3.2

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     include_package_data=True,
     packages=find_packages(include=['helenservice', 'helenservice.*']),
     url='https://github.com/carohauta/oma-helen-cli',
-    version='1.3.1',
+    version='1.3.2',
     zip_safe=False,
 )
